13f250c71db408e233021a0a34d4df443adf2ecc
Thomas Müller <mimmi20@live.de>
2024-06-22 19:46:02 +0200

    remove unused files

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -12,25 +12,10 @@ declare(strict_types = 1);
 
 namespace Mimmi20\Mezzio\BladeRenderer;
 
-use Illuminate\Contracts\View\Factory as FactoryInterface;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\View\Compilers\BladeCompiler;
-use Illuminate\View\Engines\EngineResolver;
-use Illuminate\View\FileViewFinder;
-use Jenssegers\Blade\Blade;
 use Laminas\ServiceManager\Factory\InvokableFactory;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeCompilerFactory;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeDispatcherFactory;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeFactory;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeFilesystemFactory;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeFileViewFinderFactory;
 use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeRenderer;
 use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeRendererFactory;
 use Mimmi20\Mezzio\BladeRenderer\Renderer\Container;
-use Mimmi20\Mezzio\BladeRenderer\Renderer\ViewFactoryFactory;
-use Mimmi20\Mezzio\BladeRenderer\Resolver\BladeEngineResolverBladeClosureFactory;
-use Mimmi20\Mezzio\BladeRenderer\Resolver\BladeEngineResolverFactory;
 use Mimmi20\Mezzio\BladeRenderer\Strategy\BladeStrategy;
 use Mimmi20\Mezzio\BladeRenderer\Strategy\BladeStrategyFactory;
 
@@ -60,28 +45,10 @@ final class ConfigProvider
         return [
             'aliases' => [
                 'renderer.blade' => BladeRenderer::class,
-                'blade.filesystem' => Filesystem::class,
-                'blade.dispatcher' => Dispatcher::class,
-                'blade.compiler' => BladeCompiler::class,
-                'view.engine.resolver' => EngineResolver::class,
-                'blade.file_view_finder' => FileViewFinder::class,
-
-                'blade' => Blade::class,
             ],
             'factories' => [
                 BladeRenderer::class => BladeRendererFactory::class,
                 BladeStrategy::class => BladeStrategyFactory::class,
-                Filesystem::class => BladeFilesystemFactory::class,
-                BladeCompiler::class => BladeCompilerFactory::class,
-                Dispatcher::class => BladeDispatcherFactory::class,
-                EngineResolver::class => BladeEngineResolverFactory::class,
-                FileViewFinder::class => BladeFileViewFinderFactory::class,
-                /* php closure not used when using blade for rendering but could be used this way
-                'blade.engine_resolver_php_closure' => BladeEngineResolverPHPClosureFactory::class,
-                 */
-                'blade.engine_resolver_blade_closure' => BladeEngineResolverBladeClosureFactory::class,
-                FactoryInterface::class => ViewFactoryFactory::class,
-                Blade::class => BladeFactory::class,
                 Container::class => InvokableFactory::class,
             ],
         ];
--- tests/ConfigProviderTest.php
+++ tests/ConfigProviderTest.php
@@ -12,14 +12,8 @@ declare(strict_types = 1);
 
 namespace Mimmi20\Mezzio\BladeRenderer;
 
-use Illuminate\Contracts\View\Factory as FactoryInterface;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\View\Compilers\BladeCompiler;
-use Illuminate\View\Engines\EngineResolver;
-use Illuminate\View\FileViewFinder;
-use Jenssegers\Blade\Blade;
 use Mimmi20\Mezzio\BladeRenderer\Renderer\BladeRenderer;
+use Mimmi20\Mezzio\BladeRenderer\Renderer\Container;
 use Mimmi20\Mezzio\BladeRenderer\Strategy\BladeStrategy;
 use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestCase;
@@ -45,25 +39,12 @@ final class ConfigProviderTest extends TestCase
         self::assertIsArray($factories);
         self::assertArrayHasKey(BladeRenderer::class, $factories);
         self::assertArrayHasKey(BladeStrategy::class, $factories);
-        self::assertArrayHasKey(Filesystem::class, $factories);
-        self::assertArrayHasKey(BladeCompiler::class, $factories);
-        self::assertArrayHasKey(Dispatcher::class, $factories);
-        self::assertArrayHasKey(EngineResolver::class, $factories);
-        self::assertArrayHasKey(FileViewFinder::class, $factories);
-        self::assertArrayHasKey('blade.engine_resolver_blade_closure', $factories);
-        self::assertArrayHasKey(FactoryInterface::class, $factories);
-        self::assertArrayHasKey(Blade::class, $factories);
+        self::assertArrayHasKey(Container::class, $factories);
 
         self::assertArrayHasKey('aliases', $dependencies);
         $aliases = $dependencies['aliases'];
         self::assertIsArray($aliases);
         self::assertArrayHasKey('renderer.blade', $aliases);
-        self::assertArrayHasKey('blade.filesystem', $aliases);
-        self::assertArrayHasKey('blade.dispatcher', $aliases);
-        self::assertArrayHasKey('blade.compiler', $aliases);
-        self::assertArrayHasKey('view.engine.resolver', $aliases);
-        self::assertArrayHasKey('blade.file_view_finder', $aliases);
-        self::assertArrayHasKey('blade', $aliases);
     }
 
     /** @throws Exception */
@@ -102,25 +83,12 @@ final class ConfigProviderTest extends TestCase
         self::assertIsArray($factories);
         self::assertArrayHasKey(BladeRenderer::class, $factories);
         self::assertArrayHasKey(BladeStrategy::class, $factories);
-        self::assertArrayHasKey(Filesystem::class, $factories);
-        self::assertArrayHasKey(BladeCompiler::class, $factories);
-        self::assertArrayHasKey(Dispatcher::class, $factories);
-        self::assertArrayHasKey(EngineResolver::class, $factories);
-        self::assertArrayHasKey(FileViewFinder::class, $factories);
-        self::assertArrayHasKey('blade.engine_resolver_blade_closure', $factories);
-        self::assertArrayHasKey(FactoryInterface::class, $factories);
-        self::assertArrayHasKey(Blade::class, $factories);
+        self::assertArrayHasKey(Container::class, $factories);
 
         self::assertArrayHasKey('aliases', $dependencies);
         $aliases = $dependencies['aliases'];
         self::assertIsArray($aliases);
         self::assertArrayHasKey('renderer.blade', $aliases);
-        self::assertArrayHasKey('blade.filesystem', $aliases);
-        self::assertArrayHasKey('blade.dispatcher', $aliases);
-        self::assertArrayHasKey('blade.compiler', $aliases);
-        self::assertArrayHasKey('view.engine.resolver', $aliases);
-        self::assertArrayHasKey('blade.file_view_finder', $aliases);
-        self::assertArrayHasKey('blade', $aliases);
 
         $viewManagerConfig = $config['view_manager'];
         self::assertIsArray($viewManagerConfig);
