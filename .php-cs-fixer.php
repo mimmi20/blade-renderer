67e175e0b42c5a3d0188580411ee60632ad62673
Thomas Müller <mimmi20@live.de>
2025-01-01 09:40:53 +0100

    update docBlocks

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the mimmi20/blade-renderer package.
  *
- * Copyright (c) 2024, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2025, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -58,5 +58,5 @@ return $config
             ],
         ),
     )
-    ->setUsingCache(true)
+    ->setUsingCache(false)
     ->setFinder($finder);
